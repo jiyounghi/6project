6a45e6afdf8ece7c4811867230b664c2c56e7080
phs9903 <phs9903@gmail.com>
2022-08-30 15:35:25 +0900

    2차 조정

--- FunBakery/src/main/java/com/VO/PageVO.java
+++ FunBakery/src/main/java/com/VO/PageVO.java
@@ -8,15 +8,60 @@ public class PageVO {
 	private int pageNum;
 	private int amount = 20;
 	private int total;
+	
+	
+
+	public int getStartPage() {
+		return startPage;
+	}
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
+	public int getEndPage() {
+		return endPage;
+	}
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
+	}
+	public boolean isPrev() {
+		return prev;
+	}
+	public void setPrev(boolean prev) {
+		this.prev = prev;
+	}
+	public boolean isNext() {
+		return next;
+	}
+	public void setNext(boolean next) {
+		this.next = next;
+	}
+	public int getPageNum() {
+		return pageNum;
+	}
+	public void setPageNum(int pageNum) {
+		this.pageNum = pageNum;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
 
 	public PageVO(int pageNum, int amount, int total) {
 		this.pageNum = pageNum;
 		this.amount = amount;
 		this.total = total;
 
-		this.endPage = (int) Math.ceil(this.pageNum * 0.1) * 20;
+		this.endPage = (int) Math.ceil(this.pageNum * 0.1) * 10;
 
-		this.startPage = this.endPage - 20 + 1;
+		this.startPage = this.endPage - 10 + 1;
 
 		int realEnd = (int) Math.ceil(this.total / (double) this.amount);
 
@@ -27,7 +72,5 @@ public class PageVO {
 		this.prev = this.startPage > 1;
 		
 		this.next = this.endPage < realEnd;
-		
-		System.out.println("시작페이지:" + this.startPage + ", 끝페이지:" + this.endPage);
 	}
 }
